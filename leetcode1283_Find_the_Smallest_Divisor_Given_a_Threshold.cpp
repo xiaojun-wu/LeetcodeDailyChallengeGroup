8f540ae5e8228229a646593dd5819c5422f95a90
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-05-02 23:58:49 -0400

    submit leetcode daily challenge solution 04/01/2021

---
+++ leetcode1283_Find_the_Smallest_Divisor_Given_a_Threshold.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        const int SIZE = nums.size();
+        int left = 1, right = 1000000;
+        while(left <= right){
+            double mid = left + (right - left) / 2;
+            long sum = 0;
+            for(long n : nums)
+                sum+= ceil(n / mid);
+            if(sum <= threshold)
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+        
+        return left;
+    }
+};
