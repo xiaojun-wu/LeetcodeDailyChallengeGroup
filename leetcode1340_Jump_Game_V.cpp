097b15834fb5bc3c766e11f77670873ffcd82db3
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-05-03 21:26:26 -0400

    submit leetcode daily challenge solution 05/06/2021

---
+++ leetcode1340_Jump_Game_V.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxJumps(vector<int>& arr, int d) {
+        // dp, N * d * 2
+        const int SIZE = arr.size();
+        vector<int> dp(SIZE,1);
+        vector<pair<int,int>> nums;
+        for(int i = 0;i < SIZE;i++)
+            nums.emplace_back(arr[i],i);
+        sort(nums.begin(),nums.end());
+        
+        for(int i = 0;i < SIZE;i++){
+            int idx = nums[i].second;
+            for(int j = idx - 1;j >= idx - d && j >= 0 && arr[j] < arr[idx];j--)
+                dp[idx] = max(dp[idx],dp[j] + 1);
+            for(int j = idx + 1;j <= idx + d && j < SIZE && arr[j] < arr[idx];j++)
+                dp[idx] = max(dp[idx],dp[j] + 1);
+        }
+        
+        return *max_element(dp.begin(),dp.end());
+    }
+};
