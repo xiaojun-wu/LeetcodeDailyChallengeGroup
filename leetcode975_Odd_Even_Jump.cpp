def6e00cabb86bb65340abc74293b9ac51ba1220
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-05-03 22:44:39 -0400

    submit leetcode daily challenge solution 05/09/2021

---
+++ leetcode975_Odd_Even_Jump.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int oddEvenJumps(vector<int>& arr) {
+        // dp + heap.O(NlogN)
+        const int SIZE = arr.size();
+        vector<bool> odd(SIZE,false), even(SIZE,false);
+        odd[SIZE - 1] = true;
+        even[SIZE - 1] = true;
+        map<int,int> heap;
+        heap[arr[SIZE - 1]] = SIZE - 1;
+        int res = 1;
+        
+        for(int i = SIZE - 2;i >= 0;i--){
+            auto it = heap.lower_bound(arr[i]);
+            auto it2 = heap.upper_bound(arr[i]);
+            if(it2 != heap.begin())
+                it2--;
+            if(it != heap.end())
+                odd[i] = even[it->second];
+            if(it2 != heap.end() && it2->first <= arr[i])
+                even[i] = odd[it2->second];
+            res+= odd[i];
+            heap[arr[i]] = i;
+        }
+        
+        return res;
+    }
+};
+/*
+[10,13,12,14,15]
+[2,3,1,1,4]
+[5,1,3,4,2]
+*/
