efa70e799e9dfa947b8298d8780dfced66f5b12a
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-05-03 21:12:35 -0400

    submit leetcode daily challenge 05/05/2021 solution

---
+++ leetcode1306_Jump_Game_III.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool canReach(vector<int>& arr, int start) {
+        // bfs
+        const int SIZE = arr.size();
+        if(arr[start] == 0)
+            return true;
+        vector<bool> visited(SIZE,false);
+        deque<int> q;
+        q.push_back(start);
+        visited[start] = true;
+        
+        while(!q.empty()){
+            int idx = q.front();
+            q.pop_front();
+            if(idx + arr[idx] < SIZE && !visited[idx + arr[idx]]){
+                if(arr[idx + arr[idx]] == 0)
+                    return true;
+                visited[idx + arr[idx]] = true;
+                q.emplace_back(idx + arr[idx]);
+            }
+            if(idx - arr[idx] >= 0 && !visited[idx - arr[idx]]){
+                if(arr[idx - arr[idx]] == 0)
+                    return true;
+                visited[idx - arr[idx]] = true;
+                q.emplace_back(idx - arr[idx]);
+            }
+        }
+        
+        return false;
+    }
+};
