af416b3578058f82e4531ed069c9cd7ef5d528e7
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-05-05 16:56:45 -0400

    submit leetcode daily challenge solution

---
+++ leetcode1793_Maximum_Score_of_a_Good_Subarray.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maximumScore(vector<int>& nums, int k) {
+        const int SIZE = nums.size();
+        int left = k, right = k, minVal = nums[k], res = minVal;
+        
+        while(left > 0 || right < SIZE - 1){
+            if(left > 0 && right < SIZE -1){
+                nums[left - 1] > nums[right + 1]?left--:right++;
+                minVal = min(minVal,min(nums[left],nums[right]));
+            }
+            else if(left > 0)
+                minVal = min(minVal,nums[--left]);
+            else
+                minVal = min(minVal,nums[++right]);
+            res = max(res,(right - left + 1) * minVal);
+        }
+        
+        return res;
+    }
+};
