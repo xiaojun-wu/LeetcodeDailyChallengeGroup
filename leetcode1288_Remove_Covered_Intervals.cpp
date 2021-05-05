9aeb495229a836ba126e9fef1d6902de2dbf09f8
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-05-05 17:44:35 -0400

    submit leetcode daily challenge solution

---
+++ leetcode1288_Remove_Covered_Intervals.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        const int SIZE = intervals.size();
+        sort(intervals.begin(),intervals.end(),[](auto& lhs,auto& rhs){
+            if(lhs[0] != rhs[0])
+                return lhs[0] < rhs[0];
+            return lhs[1] > rhs[1];
+        });
+        int remove = 0, idx = 0;
+        
+        for(int i = 1;i < SIZE;i++){
+            if(intervals[i][1] <= intervals[idx][1])
+                remove++;
+            else
+                idx = i;
+        }
+        
+        return SIZE - remove;
+    }
+};
