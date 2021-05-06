7bb9dc32cdecf33edfc745d746ca7611fd6b3788
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-05-06 01:51:36 -0400

    submit leetcode daily challenge solution

---
+++ leetcode56_Merge_Intervals.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        const int SIZE = intervals.size();
+        vector<vector<int>> res;
+        sort(intervals.begin(),intervals.end(),[](auto& lhs,auto& rhs){
+            if(lhs[0] != rhs[0])
+                return lhs[0] < rhs[0];
+            return lhs[1] < rhs[1];
+        });
+        
+        res.push_back(intervals[0]);
+        
+        for(int i = 1;i < SIZE;i++){
+            if(res.back()[1] >= intervals[i][0])
+                res.back()[1] = max(res.back()[1],intervals[i][1]);
+            else
+                res.push_back(intervals[i]);
+        }
+        
+        return res;
+    }
+};
