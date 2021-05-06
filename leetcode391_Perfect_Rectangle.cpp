0aee1b7a89eb3df482d52e7b8530335d9470da96
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-05-06 01:36:53 -0400

    submit leetcode daily challenge solution

---
+++ leetcode391_Perfect_Rectangle.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isRectangleCover(vector<vector<int>>& rectangles) {
+        set<pair<int,int>> heap;
+        long long area = 0;
+        
+        for(vector<int>& r : rectangles){
+            vector<pair<int,int>> ps;
+            ps.emplace_back(r[0],r[1]);// bottom-left
+            ps.emplace_back(r[0],r[3]);// top-left
+            ps.emplace_back(r[2],r[1]);// bottom-right
+            ps.emplace_back(r[2],r[3]);// top-right
+            for(auto &p : ps){
+                auto it = heap.insert(p);
+                if(!it.second)
+                    heap.erase(it.first);
+            }
+            area+= (ps[3].first - ps[0].first) * (ps[3].second - ps[0].second);
+        }
+        if(heap.size() != 4)
+            return false;
+        auto p1 = *(heap.begin());
+        auto p2 = *(heap.rbegin());
+        return area == (p2.first - p1.first) * (p2.second - p1.second);
+    }
+};
