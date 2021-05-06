c3a2d83bde34458d44d2bfc2dcf2f51e65bbf741
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-05-06 02:24:03 -0400

    submit leetcode challenge solution

---
+++ leetcode57_Insert_Interval.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        const int SIZE = intervals.size();
+        vector<vector<int>> res;
+        int left = newInterval[0], right = newInterval[1], idx = 0;
+        
+        while(idx < SIZE && intervals[idx][1] < left)
+            res.push_back(intervals[idx++]);
+        
+        while(idx < SIZE && intervals[idx][0] <= right){
+            left = min(left,intervals[idx][0]);
+            right = max(right,intervals[idx++][1]);
+        }
+        
+        res.push_back({left,right});
+        
+        while(idx < SIZE){
+            res.push_back(intervals[idx++]);
+        }
+        
+        return res;
+    }
+};
