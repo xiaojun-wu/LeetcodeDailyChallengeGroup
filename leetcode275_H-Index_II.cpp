ad44aba0cff6c29c0ee62593fd0e7c43bf2e51cd
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-05-10 19:49:39 -0400

    submit leetcode daily challenge solution 05/11/2021

---
+++ leetcode275_H-Index_II.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        // binary search
+        const int SIZE = citations.size();
+        int left = 0, right = SIZE - 1, res = 0;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(citations[mid] <= SIZE - mid){
+                left = mid + 1;
+                res = max(res,citations[mid]);
+            }
+            else{
+                right = mid - 1;
+                res = max(res,SIZE - mid);
+            }
+        }
+        return res;
+    }
+};
