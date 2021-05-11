23e5abd9cab84efe64bf43df487e9ad9856033fa
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-05-11 01:03:37 -0400

    submit leetcode daily challenge solution 05/15/2021

--- leetcode1402_Reducing_Dishes.cpp
+++ leetcode1402_Reducing_Dishes.cpp
@@ -3,16 +3,13 @@ public:
     int maxSatisfaction(vector<int>& satisfaction) {
         const int SIZE = satisfaction.size();
         sort(satisfaction.begin(),satisfaction.end());
-        int res = INT_MIN;
-        for(int i = 0;i < SIZE;i++){
-            int sum = 0;
-            for(int j = i,k = 1;j < SIZE;j++,k++)
-                sum+= satisfaction[j] * k;
-            if(sum < res) break;
-            res = max(res,sum);
+        int res = 0, presum = 0;
+        for(int i = SIZE - 1;i >= 0 && satisfaction[i] + presum > 0;i--){
+            presum+= satisfaction[i];
+            res+= presum;
         }
         
-        return res < 0?0:res;
+        return res;
     }
 };
 // https://tinyurl.com/ut9963tc
