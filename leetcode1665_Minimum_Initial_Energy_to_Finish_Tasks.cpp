52237e9cefa45d6dd56a00ce91a33b42fb36dffb
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-05-11 00:23:16 -0400

    submit leetcode daily challenge solution 05/14/2021

---
+++ leetcode1665_Minimum_Initial_Energy_to_Finish_Tasks.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minimumEffort(vector<vector<int>>& tasks) {
+        /*
+        choose the larger difference first?
+        */
+        const int SIZE = tasks.size();
+        sort(tasks.begin(),tasks.end(),[](auto& lhs,auto& rhs){
+            if(lhs[1] - lhs[0] != rhs[1] - rhs[0])
+                return lhs[1] - lhs[0] > rhs[1] - rhs[0];
+            return lhs[1] > rhs[1];
+        });
+        int res = 0, cur = 0;
+        
+        for(int i = 0;i < SIZE;i++){
+            if(cur < tasks[i][1]){
+                res+= tasks[i][1] - cur;
+                cur = tasks[i][1];
+            }
+            cur-= tasks[i][0];
+        }
+        
+        return res;
+    }
+};
