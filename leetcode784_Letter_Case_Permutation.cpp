66cc2a7cec26cfbb7af2995ffcd312caf5ef3973
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-05-11 22:01:59 -0400

    submit leetcode daily challenge solution

---
+++ leetcode784_Letter_Case_Permutation.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<string> letterCasePermutation(string s) {
+        // backtracking.
+        const int SIZE = s.length();
+        vector<string> res;
+        
+        function<void(int)> dfs = [&](int idx) {
+            if(idx == SIZE){
+                res.push_back(s);
+                return;
+            }
+            if(!('0' <= s[idx] && s[idx] <= '9')){
+                if(s[idx] > 'Z')
+                    s[idx]-= 32;
+                dfs(idx + 1);
+                s[idx]+= 32;
+                dfs(idx + 1);
+            }
+            else
+                dfs(idx + 1);
+        };
+        
+        dfs(0);
+        
+        return res;
+    }
+};
