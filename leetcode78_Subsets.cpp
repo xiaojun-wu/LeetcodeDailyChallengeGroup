636bb88923d6a5d0355de1d552a617b71b49a3ee
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-05-12 21:43:45 -0400

    submit leetcode daily challenge solution

---
+++ leetcode78_Subsets.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        const int SIZE = nums.size();
+        vector<vector<int>> res;
+        vector<int> cur;
+        
+        function<void(int)> dfs = [&](int idx) {
+            if(idx == SIZE){
+                res.push_back(cur);
+                return;
+            }
+            dfs(idx + 1);
+            cur.push_back(nums[idx]);
+            dfs(idx + 1);
+            cur.pop_back();
+        };
+        
+        dfs(0);
+        
+        return res;
+    }
+};
