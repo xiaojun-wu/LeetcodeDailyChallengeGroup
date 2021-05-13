3b1805e253bb24ef04f0a78f3aa7a6f4e617f193
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-05-12 23:40:36 -0400

    submit leetcode dailyc challenge solution

---
+++ leetcode17_Letter_Combinations_of_a_Phone_Number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        const int LEN = digits.length();
+        vector<string> mp = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        vector<string> res;
+        string cur = "";
+        
+        if(LEN == 0)
+            return res;
+        
+        function<void()> dfs = [&]() {
+            if(cur.length() == LEN)
+                return res.push_back(cur);
+            for(char c : mp[digits[cur.length()] - '0']){
+                cur+= c;
+                dfs();
+                cur.pop_back();
+            }
+        };
+        
+        dfs();
+        
+        return res;
+    }
+};
