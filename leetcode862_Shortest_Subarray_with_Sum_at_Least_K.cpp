e967e60f8ded8f2766c3624ac2c626c7dad1af19
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-05-16 22:26:13 -0400

    submit leetcode daily challenge solution

---
+++ leetcode862_Shortest_Subarray_with_Sum_at_Least_K.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int shortestSubarray(vector<int>& A, int K) {
+        const int SIZE = A.size();
+        int res = SIZE + 1;
+        long long sum = 0;
+        vector<long long> presum(1,0);
+        deque<int> q;
+        q.push_back(0);
+        
+        for(int i = 0;i < SIZE;i++){
+            sum+= (long long)(A[i]);
+            while(!q.empty() && sum - presum[q.back()] >= K){
+                res = min(res,i - q.back() + 1);
+                q.pop_back();
+            }
+            presum.push_back(sum);
+            while(!q.empty() && presum[q.front()] >= presum[i + 1])
+                q.pop_front();
+            q.emplace_front(i + 1);
+        }
+        
+        return res == SIZE + 1?-1:res;
+    }
+};
