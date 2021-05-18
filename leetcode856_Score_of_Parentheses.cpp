c73976b4fdb4874c99ba10858a5acad70a37d035
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-05-17 20:35:54 -0400

    submit leetcode daily challenge soluion 05/17/2021

---
+++ leetcode856_Score_of_Parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int scoreOfParentheses(string S) {
+        vector<int> stack;
+        int res = 0;
+        
+        for(char c : S){
+            if(c == '(')
+                stack.push_back(0);
+            else{
+                int val = 0;
+                while(!stack.empty() & stack.back() != 0){
+                    val+= stack.back();
+                    stack.pop_back();
+                }
+                stack.pop_back();
+                val = val==0?1:val*2;
+                stack.push_back(val);
+            }
+        }
+        
+        while(!stack.empty()){
+            res+= stack.back();
+            stack.pop_back();
+        }
+        
+        return res;
+    }
+};
