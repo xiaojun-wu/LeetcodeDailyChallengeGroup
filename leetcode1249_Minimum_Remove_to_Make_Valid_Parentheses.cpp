cb9071148224035547e7a79bf0882ffb8c74621d
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-05-18 00:51:40 -0400

    submit leetcode daily challenge solution 05/20/2021

---
+++ leetcode1249_Minimum_Remove_to_Make_Valid_Parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        const int LEN = s.length();
+        string res = "";
+        vector<int> stack;
+        unordered_set<int> toRemove;
+        
+        for(int i = 0;i < LEN;i++){
+            if(s[i] == '(')
+                stack.push_back(i);
+            else if(s[i] == ')'){
+                if(stack.empty())
+                    toRemove.insert(i);
+                else
+                    stack.pop_back();
+            }
+        }
+        
+        toRemove.insert(stack.begin(),stack.end());
+        
+        for(int i = 0;i < LEN;i++)
+            if(!toRemove.count(i))
+                res+= s[i];
+        
+        return res;
+    }
+};
