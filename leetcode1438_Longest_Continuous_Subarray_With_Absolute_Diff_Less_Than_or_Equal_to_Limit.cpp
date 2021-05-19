c82f64d67bb5b63114f63d0e563b21bf86f7522c
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-05-19 02:23:22 -0400

    submit leetcode daily challenge solution

---
+++ leetcode1438_Longest_Continuous_Subarray_With_Absolute_Diff_Less_Than_or_Equal_to_Limit.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums, int limit) {
+        const int SIZE = nums.size();
+        deque<int> minStack, maxStack;
+        int res = 1;
+        
+        for(int i = 0, j = 0;i < SIZE;i++){
+            while(!minStack.empty() && nums[minStack.back()] > nums[i])
+                minStack.pop_back();
+            while(!maxStack.empty() && nums[maxStack.back()] < nums[i])
+                maxStack.pop_back();
+            minStack.push_back(i);
+            maxStack.push_back(i);
+            while(j < i && nums[maxStack.front()] - nums[minStack.front()] > limit){
+                while(!maxStack.empty() && maxStack.front() <= j)
+                    maxStack.pop_front();
+                while(!minStack.empty() && minStack.front() <= j)
+                    minStack.pop_front();
+                j++;
+            }
+            res = max(res,i - j + 1);
+        }
+        
+        return res;
+    }
+};
