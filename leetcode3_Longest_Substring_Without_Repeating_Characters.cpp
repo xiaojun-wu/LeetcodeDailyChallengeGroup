9a9269c1fa2c7d372efa24c549c823d6047ac950
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-05-22 21:04:28 -0400

    submit leetcode daily challenge solution

---
+++ leetcode3_Longest_Substring_Without_Repeating_Characters.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        const int SIZE = s.length();
+        vector<int> pos(256,-1);
+        int res = 0;
+        
+        for(int r = 0,l = 0;r < SIZE;r++){
+            if(pos[s[r]] >= l)
+                l = pos[s[r]] + 1;
+            pos[s[r]] = r;
+            res = max(res,r - l + 1);
+        }
+        
+        return res;
+    }
+};
