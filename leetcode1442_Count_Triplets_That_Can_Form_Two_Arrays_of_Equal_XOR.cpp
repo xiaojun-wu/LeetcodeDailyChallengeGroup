13b082997864b2c687087d8773bd0dddf5c0bb0d
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-05-24 18:16:08 -0400

    submit leetcode daily challenge solution 05/25/2021

---
+++ leetcode1442_Count_Triplets_That_Can_Form_Two_Arrays_of_Equal_XOR.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int countTriplets(vector<int>& arr) {
+        /*
+        find the number of subarray xor is cur;
+        TC: O(N), SC: O(N)
+        */
+        const int SIZE = arr.size();
+        unordered_map<int,int> totalLens;
+        unordered_map<int,int> count;
+        totalLens[0] = -1;
+        count[0] = 1;
+        int res = 0;
+        
+        for(int i = 0,x = 0;i < SIZE;i++){
+            x^= arr[i];
+            if(totalLens.count(x))
+                res+= ((i - 1) * count[x] - totalLens[x]);
+            totalLens[x]+= i;
+            count[x]++;
+        }
+        
+        return res;
+    }
+};
