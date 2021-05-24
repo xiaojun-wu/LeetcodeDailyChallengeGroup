64f8ae7d4bf60abbc8557484abd6b42e070729eb
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-05-24 17:54:28 -0400

    submit leetcode daily challenge solution 05/24/2021

---
+++ leetcode930_Binary_Subarrays_With_Sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        // dp, TC: O(N), SC: O(N)
+        const int SIZE = nums.size();
+        vector<int> dp(SIZE + 1,0);
+        dp[0] = 1;
+        int presum = 0, res = 0;
+        
+        for(int n : nums){
+            presum+= n;
+            if(presum >= goal)
+                res+= dp[presum - goal];
+            dp[presum]++;
+        }
+        
+        return res;
+    }
+};
