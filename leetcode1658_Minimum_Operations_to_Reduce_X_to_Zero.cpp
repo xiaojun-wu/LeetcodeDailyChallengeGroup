b55f4d5016379fd81b1380b41ab6a8f37de3e3ba
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-05-24 20:14:11 -0400

    submit leetcode daily challenge solution 05/29/2021

---
+++ leetcode1658_Minimum_Operations_to_Reduce_X_to_Zero.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+        /*
+        find longest subarray with sum equals to sum(nums) - x.
+        */
+        const int SIZE = nums.size();
+        int res = -1;
+        int totalsum = accumulate(nums.begin(),nums.end(),0), cursum = 0;
+        int target = totalsum - x;
+        if(target < 0)
+            return -1;
+        
+        for(int l = 0,r = 0;r < SIZE;r++){
+            cursum+= nums[r];
+            while(cursum > target)
+                cursum-= nums[l++];
+            if(cursum == target)
+                res = max(res,r - l + 1);
+        }
+        
+        return res == -1?-1:SIZE - res;
+    }
+};
