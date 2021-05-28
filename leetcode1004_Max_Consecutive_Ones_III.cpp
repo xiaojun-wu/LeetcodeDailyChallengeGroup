9b1d9b5c17d15d44e31765d2999fead7e45cd804
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-05-28 00:33:00 -0400

    submit leetcode daily chellenge solution

---
+++ leetcode1004_Max_Consecutive_Ones_III.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& A, int K) {
+        const int SIZE = A.size();
+        deque<int> q;
+        int res = 0;
+        
+        for(int l = 0,r = 0;r < SIZE;r++){
+            if(A[r] == 0)
+                q.push_back(r);
+            if(q.size() <= K)
+                res = max(res,r - l + 1);
+            else{
+                l = q.front() + 1;
+                q.pop_front();
+            }
+        }
+        
+        return res;
+    }
+};
