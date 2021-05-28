50b1e8faa5f9d98668a72623a2c433e98bec75d8
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-05-28 02:48:30 -0400

    submit leetcode daily challenge solution

---
+++ leetcode1248_Count_Number_of_Nice_Subarrays.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        const int SIZE = nums.size();
+        deque<int> q;
+        int res = 0, l = 0,r = 0;
+        
+        for(;r < SIZE;r++){
+            if(nums[r] % 2 == 0) continue;
+            if(q.size() + 1 > k){
+                res+= (q.front() - l + 1) * (r - q.back());
+                l = q.front() + 1;
+                q.pop_front();
+            }
+            q.push_back(r);
+        }
+        
+        if(q.size() == k)
+            res+= (q.front() - l + 1) * (r - q.back());
+        
+        return res;
+    }
+};
