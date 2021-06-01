a9014269778c1fb6c006ff85cb35d3f9dab05921
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-06-01 03:06:00 -0400

    submit leetcode daily challenge solution 06/03/2021

---
+++ leetcode1648_Sell_Diminishing-Valued_Colored_Balls.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& inventory, int orders) {
+        /*
+        greedy + sort
+        TC: O(NlogN), SC: O(N)
+        */
+        const int SIZE = inventory.size(), MOD = 1e9 + 7;
+        sort(inventory.begin(),inventory.end(),greater<int>());
+        inventory.push_back(0);
+        int res = 0;
+        
+        function<long(long,long)> getProfit = [&](long h,long l) ->long {
+            return (h * (h + 1) / 2) - (l * (l + 1) / 2);
+        };
+        
+        for(long i = 0;i < SIZE && orders > 0;i++){
+            if((long)(inventory[i] - inventory[i + 1]) * (i + 1) <= orders){
+                res = (res + getProfit(inventory[i],inventory[i + 1]) * (i + 1)) % MOD;
+                orders-= (inventory[i] - inventory[i + 1]) * (i + 1);
+            }
+            else{
+                long level = orders / (i + 1), rest = orders % (i + 1);
+                res = (res + getProfit(inventory[i],inventory[i] - level) * (i + 1)) % MOD;
+                res = (res + rest * (inventory[i] - level)) % MOD;
+                orders = 0;
+            }
+        }
+        
+        return res;
+    }
+};
