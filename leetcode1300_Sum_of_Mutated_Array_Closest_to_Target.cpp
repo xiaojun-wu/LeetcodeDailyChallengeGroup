4ad16d0bd84bfded39435b4119554c588710e820
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-06-01 03:58:36 -0400

    submit leetcode daily challenge solution

---
+++ leetcode1300_Sum_of_Mutated_Array_Closest_to_Target.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int findBestValue(vector<int>& arr, int target) {
+        const int SIZE = arr.size();
+        sort(arr.begin(),arr.end());
+        vector<int> presum(SIZE + 1,0);
+        int left = 0, right = arr.back(), val = -1, res = -1;
+        
+        for(int i = 0;i < SIZE;i++)
+            presum[i + 1]+= presum[i] + arr[i];
+        
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            int idx = upper_bound(arr.begin(),arr.end(),mid) - arr.begin();
+            int sum = presum[idx] + (SIZE - idx) * mid;
+            int diff1 = abs(val - target), diff2 = abs(sum - target);
+            if(diff1 > diff2 || (diff1 == diff2 && res > mid)){
+                res = mid;
+                val = sum;
+            }
+            if(sum < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        
+        return res;
+    }
+};
