858e845d550d2a14af5ef02d057a2cc193a14103
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-06-08 12:04:24 -0400

    submit leetcode daily challenge solution 06/09/2021

---
+++ leetcode1675_Minimize_Deviation_in_Array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minimumDeviation(vector<int>& nums) {
+        const int SIZE = nums.size();
+        priority_queue<int> pq;
+        int minVal = INT_MAX;
+        int res = INT_MAX;
+        
+        for(int n : nums){
+            if(n % 2 == 1)
+                n*= 2;
+            minVal = min(minVal,n);
+            pq.push(n);
+        }
+        
+        while(pq.top() % 2 == 0){
+            int n = pq.top();
+            res = min(res,n - minVal);
+            pq.pop();
+            if(n % 2 == 0)
+                n/= 2;
+            minVal = min(minVal,n);
+            pq.push(n);
+        }
+        
+        return min(res,pq.top() - minVal);
+    }
+};
