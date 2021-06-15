c59a13e3e12dd5ad8c2977c0e6f8732f6df33c86
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-06-15 18:47:02 -0400

    submit leetcode daily challenge solution 06/19/2021

---
+++ leetcode1673_Find_the_Most_Competitive_Subsequence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> mostCompetitive(vector<int>& nums, int k) {
+        /*
+        monotonic stack
+        TC: O(N), SC: O(N).
+        */
+        const int SIZE = nums.size();
+        vector<int> res;
+        
+        for(int i = 0;i < SIZE;i++){
+            while(!res.empty() && res.back() > nums[i] && res.size() + (SIZE - i) > k)
+                res.pop_back();
+            if(res.size() < k)
+                res.push_back(nums[i]);
+        }
+        
+        return res;
+    }
+};
