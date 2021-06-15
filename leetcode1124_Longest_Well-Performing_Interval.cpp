acf7fc318fb9ce602ce76bca5907f2f5611fa9d3
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-06-15 18:34:43 -0400

    submit leetcode daily challenge solution 06/18/2021

---
+++ leetcode1124_Longest_Well-Performing_Interval.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestWPI(vector<int>& hours) {
+        /*
+        prefix sum
+        TC:O(N), SC:O(10002)
+        */
+        const int SIZE = hours.size();
+        vector<int> mp(10002,-1);
+        int sum = 0, res = 0;
+        
+        for(int i = 0;i < SIZE;i++){
+            sum+= hours[i] > 8?1:-1;
+            if(sum > 0)
+                res = i + 1;
+            else{
+                if(mp[-sum + 1] != -1)
+                    res = max(res,i - mp[-sum + 1]);
+                if(mp[-sum] == -1)
+                    mp[-sum] = i;
+            }
+        }
+        
+        return res;
+    }
+};
