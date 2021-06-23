d3603de798becffea33f0ae6abcd09bc4120309a
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-06-22 20:40:03 -0400

    submit leetcode daily challenge solution 06/26/2021

--- leetcode1537_Get_the_Maximum_Score.cpp
+++ leetcode1537_Get_the_Maximum_Score.cpp
@@ -1,28 +1,33 @@
 class Solution {
 public:
     int maxSum(vector<int>& nums1, vector<int>& nums2) {
+        /*
+        two pointers.
+        TC : O(M + N)
+        SC : O(1)
+        */
         const int SIZE1 = nums1.size(), SIZE2 = nums2.size(), MOD = 1e9 + 7;
-        int idx1 = 0, idx2 = 0;
-        long long sum1 = 0, sum2 = 0;
+        int res = 0;
+        int idx1 = SIZE1 - 1,idx2 = SIZE2 - 1;
+        long long pre1 = 0, pre2 = 0;
         
-        while(idx1 < SIZE1 && idx2 < SIZE2){
-            if(nums1[idx1] < nums2[idx2])
-                sum1+= nums1[idx1++];
-            else if(nums1[idx1] > nums2[idx2])
-                sum2+= nums2[idx2++];
+        while(idx1 >= 0 && idx2 >= 0){
+            if(nums1[idx1] > nums2[idx2])
+                pre1+= (long long)nums1[idx1--];
+            else if(nums1[idx1] < nums2[idx2])
+                pre2+= (long long)nums2[idx2--];
             else{
-                sum1 = max(sum1,sum2) + nums1[idx1];
-                sum2 = sum1;
-                idx1++;
-                idx2++;
+                pre1 = pre2 = max(pre1,pre2) + (long long)(nums1[idx1]);
+                idx1--;
+                idx2--;
             }
         }
-        while(idx1 < SIZE1)
-            sum1+= nums1[idx1++];
-        while(idx2 < SIZE2)
-            sum2+= nums2[idx2++];
+        while(idx1 >= 0)
+            pre1+= (long long)nums1[idx1--];
+        while(idx2 >= 0)
+            pre2+= (long long)nums2[idx2--];
         
-        return max(sum1,sum2) % MOD;
+        return max(pre1,pre2) % MOD;
     }
 };
 // https://tinyurl.com/7bf3d5ps
