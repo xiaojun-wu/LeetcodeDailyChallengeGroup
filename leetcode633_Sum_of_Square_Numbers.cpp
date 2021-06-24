2555fecbd55225ccf5177752c4f9a43741568685
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-06-24 00:00:16 -0400

    submit leetcode daily challenge solution

--- leetcode633_Sum_of_Square_Numbers.cpp
+++ leetcode633_Sum_of_Square_Numbers.cpp
@@ -1,14 +1,38 @@
 class Solution {
 public:
     bool judgeSquareSum(int c) {
-        unordered_set<int> nums;
-        for(int i = 0;i <= 46340 && i * i <= c;i++){
-            nums.insert(i * i);
+        /*
+        two pointers + binary search
+        TC : O(sqrt(c) * log(sqrt(c)))
+        SC : O(1);
+        */
+//         const int LIMIT = sqrt(c);
+//         for(int i = 0;i <= LIMIT;i++){
+//             int target = c - i * i;
+//             int left = 0, right = LIMIT;
+//             while(left <= right){
+//                 int mid = left + (right - left) / 2, val = mid * mid;
+//                 if(val == target)
+//                     return true;
+//                 else if(val < target)
+//                     left = mid + 1;
+//                 else
+//                     right = mid - 1;
+//             }
+//         }
+        
+//         return false;
+        
+        /*
+        math
+        TC : O(sqrt(c))
+        SC : O(1);
+        */
+        const int LIMIT = sqrt(c);
+        for(int i = 0;i <= LIMIT;i++){
+            double n = sqrt(c - i * i);
+            if((int)n == n) return true;
         }
-        for(auto it = nums.begin();it != nums.end();it++){
-            if(nums.count(c - *it)) return true;
-        }
-
         return false;
     }
 };
