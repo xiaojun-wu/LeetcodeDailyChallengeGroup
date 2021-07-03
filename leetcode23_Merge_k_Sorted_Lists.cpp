ef1b553c7ac2387cce33d276d537e96a107965bf
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-07-02 23:33:14 -0400

    submit leetcode daily challenge solution 07/02/2021

--- leetcode23_Merge_k_Sorted_Lists.cpp
+++ leetcode23_Merge_k_Sorted_Lists.cpp
@@ -12,28 +12,56 @@ class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         /*
-        priority_queue
-        TC : O(N logN)
-        SC : O(N)
+        merge with recursion
+        merge half of lists in given range [left,right]
+        if left == right, return lists[left].
+        if left + 1 == right, merge lists[left] and lists[right] and return.
+        otherwise, keep splite lists.
+        
+        TC : O(K * N * logK)
+        SC : O(log K)
         */
         const int SIZE = lists.size();
-        ListNode* pre = new ListNode(0), *cur = pre;
-        priority_queue<pair<int,ListNode*>,vector<pair<int,ListNode*>>,greater<pair<int,ListNode*>>> pq;
-        
-        for(int i = 0;i < SIZE;i++)
-            if(lists[i] != nullptr)
-                pq.emplace(lists[i]->val,lists[i]);
-        
-        while(!pq.empty()){
-            auto[val,node] = pq.top();
-            pq.pop();
-            cur->next = node;
-            if(node->next)
-                pq.emplace(node->next->val,node->next);
-            cur = cur->next;
+        if(SIZE == 0)
+            return nullptr;
+
+        function<ListNode*(int,int)> spliteLists = [&](int left,int right) ->ListNode*{
+            if(left == right)
+                return lists[left];
+            int mid = left + (right - left) / 2;
+            ListNode *l1 = nullptr, *l2 = nullptr;
+            if(mid - left <= 1){
+                if(mid - left == 0) l1 = lists[left];
+                else l1 = mergetTwoLists(lists[left],lists[left + 1]);
+            }
+            else
+                l1 = spliteLists(left,mid);
+            if(right - mid - 1 <= 1){
+                if(right - mid - 1 == 0) l2 = lists[right];
+                else l2 = mergetTwoLists(lists[mid + 1],lists[right]);
+            }
+            else
+                l2 = spliteLists(mid + 1,right);
+            return mergetTwoLists(l1,l2);
+        };
+
+        return spliteLists(0,SIZE - 1);
+    }
+    ListNode* mergetTwoLists(ListNode* l1,ListNode* l2){
+        if(l1 == nullptr || l2 == nullptr){
+            return l1 == nullptr?l2:l1;
         }
-        
+        ListNode* pre = new ListNode(0), *cur = pre;
+        while(l1 && l2){
+            if(l1->val > l2->val)
+                swap(l1,l2);
+            cur->next = l1;
+            cur = cur->next;
+            l1 = l1->next;
+        }
+        cur->next = l2;
         return pre->next;
     }
 };
+
 // https://tinyurl.com/3p8kvnjt
