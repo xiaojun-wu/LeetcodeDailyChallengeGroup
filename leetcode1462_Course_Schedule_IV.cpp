c0a2ffa6c6bdb9a00477cd50a543f5d366e6a976
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-07-03 20:56:39 -0400

    submit leetcode daily challenge solution

---
+++ leetcode1462_Course_Schedule_IV.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
+        vector<vector<int>> adjacents(numCourses,vector<int>());
+        vector<vector<int>> pre(numCourses,vector<int>(numCourses,0));
+        vector<int> indegree(numCourses,0);
+        vector<bool> res;
+        deque<int> q;
+        
+        for(vector<int>& p : prerequisites){
+            adjacents[p[0]].push_back(p[1]);
+            indegree[p[1]]++;
+        }
+        
+        for(int i = 0;i < numCourses;i++)
+            if(indegree[i] == 0)
+                q.push_back(i);
+        
+        while(!q.empty()){
+            for(int i = q.size();i > 0;i--){
+                int node = q.front();
+                // cout<<node<<" ";
+                q.pop_front();
+                for(int nei : adjacents[node]){
+                    indegree[nei]--;
+                    if(indegree[nei] == 0)
+                        q.push_back(nei);
+                    for(int j = 0;j < numCourses;j++)
+                        pre[nei][j]|= pre[node][j];
+                    pre[nei][node] = 1;
+                }
+            }
+        }
+        
+        for(vector<int>& query : queries){
+            if(pre[query[1]][query[0]])
+                res.push_back(true);
+            else
+                res.push_back(false);
+        }
+        
+        return res;
+    }
+};
