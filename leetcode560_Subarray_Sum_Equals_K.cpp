9e15f75f9d89c10a0b2e4121d8fd077257f6bdf6
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-07-06 16:48:40 -0400

    submit leetcode daily challenge solution 07/11/2021

---
+++ leetcode560_Subarray_Sum_Equals_K.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        const int SIZE = nums.size();
+        unordered_map<int,int> mp;
+        int res = 0;
+        mp[0] = 1;
+        
+        for(int i = 0,sum = 0;i < SIZE;i++){
+            sum+= nums[i];
+            if(mp.count(sum - k))
+                res+= mp[sum - k];
+            mp[sum]++;
+        }
+        
+        return res;
+    }
+};
