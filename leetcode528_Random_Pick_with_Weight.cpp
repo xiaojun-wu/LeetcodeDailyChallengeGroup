f46c2fed87add4d53b0588526f39746991f6c9df
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-07-06 16:42:35 -0400

    submit leetcode daily challenge solution 07/09/2021

---
+++ leetcode528_Random_Pick_with_Weight.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    Solution(vector<int>& w) {
+        presum = vector<int>(1,0);
+        
+        for(int n : w)
+            presum.push_back(presum.back() + n);
+        
+        srand(time(NULL));
+    }
+    
+    int pickIndex() {
+        int sum = presum.back();
+        int pickNum = rand() % sum + 1;
+        int idx = lower_bound(presum.begin(),presum.end(),pickNum) - presum.begin() - 1;
+        
+        return idx;
+    }
+private:
+    vector<int> presum;
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(w);
+ * int param_1 = obj->pickIndex();
+ */
