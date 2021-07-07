d6d0b9ca8b724e37a999a939eb780d91b6a80a17
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-07-07 17:53:11 -0400

    submit leetcode daily challenge solution

--- leetcode1316_Distinct_Echo_Substrings.cpp
+++ leetcode1316_Distinct_Echo_Substrings.cpp
@@ -3,24 +3,25 @@ class Solution {
 public:
     int distinctEchoSubstrings(string text) {
         const int LEN = text.length();
-        vector<vector<ULL>> dp(LEN,vector<ULL>(LEN,0));
+        vector<ULL> presum(1,0);
+        vector<ULL> pow(1,1);
         const ULL BASE = 29;
         int res = 0;
         
-        for(int l = 0;l < LEN;l++){
-            ULL hash = 0;
-            for(int r = l;r < LEN;r++){
-                hash = hash * BASE + (ULL)(text[r] - 'a' + 1);
-                dp[l][r] = hash;
-            }
+        for(int i = 0;i < LEN;i++){
+            presum.push_back(presum.back() * BASE + (ULL)(text[i] - 'a' + 1));
+            pow.push_back(pow.back() * BASE);
         }
         
         for(int len = 2;len <= LEN;len+= 2){
             unordered_set<ULL> seen;
             for(int r = len - 1;r < LEN;r++){
                 int l = r - len + 1;
-                if(dp[l][l + len / 2 - 1] == dp[l + len / 2][r])
-                    seen.insert(dp[l][r]);
+                ULL leftVal = presum[l + len / 2] - presum[l] * pow[len / 2];
+                ULL rightVal = presum[r + 1] - presum[l + len / 2] * pow[len / 2];
+                ULL val = presum[r + 1] - presum[l] * pow[len];
+                if(leftVal == rightVal)
+                    seen.insert(val);
             }
             res+= seen.size();
         }
