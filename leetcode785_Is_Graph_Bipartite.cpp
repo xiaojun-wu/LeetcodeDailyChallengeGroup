852a25218ae9e3980364d456298b5887b18a0a0b
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-07-10 19:27:36 -0400

    submit leetcode daily challenge solution

---
+++ leetcode785_Is_Graph_Bipartite.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        const int SIZE = graph.size();
+        vector<int> colors(SIZE,0);
+        
+        function<bool(int,int)> dfs = [&](int node,int color) ->bool {
+            colors[node] = color;
+            for(int nei : graph[node]){
+                if(colors[nei] == color)
+                    return false;
+                if(colors[nei] == 0 && !dfs(nei,-color))
+                    return false;
+            }
+            return true;
+        };
+        
+        for(int i = 0;i < SIZE;i++)
+            if(colors[i] == 0 && !dfs(i,1))
+                return false;
+        
+        return true;
+    }
+};
