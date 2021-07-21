09aa12a7ba8e99ae8aab4c98cbe898a77c5c07c7
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-07-20 21:33:54 -0400

    submit leetcode daily challenge solution 07/19/2021

---
+++ leetcode384_Shuffle_an_Array.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    Solution(vector<int>& nums) {
+        original = nums;
+        cur = nums;
+        srand(time(nullptr));
+    }
+    
+    /** Resets the array to its original configuration and return it. */
+    vector<int> reset() {
+        return original;
+    }
+    
+    /** Returns a random shuffling of the array. */
+    vector<int> shuffle() {
+        for(int i = cur.size() - 1;i > 0;i--){
+            int idx = rand() & i;
+            swap(cur[idx],cur[i]);
+        }
+        return cur;
+    }
+private:
+    vector<int> original;
+    vector<int> cur;
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(nums);
+ * vector<int> param_1 = obj->reset();
+ * vector<int> param_2 = obj->shuffle();
+ */
