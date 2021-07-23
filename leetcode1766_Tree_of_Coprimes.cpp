278e216a0f6b60473beda6d0a2c2e2c2b5f36f3c
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-07-23 06:50:15 -0400

    submit leetcode daily challenge solution 07/25/2021

--- leetcode1766_Tree_of_Coprimes.cpp
+++ leetcode1766_Tree_of_Coprimes.cpp
@@ -4,7 +4,7 @@ public:
         const int SIZE = nums.size();
         vector<vector<int>> adjacents(SIZE,vector<int>());
         int isGCD[51][51] = {0};
-        unordered_map<int,int> mp;
+        int mp[51] = {0};
         vector<int> visitedTime(SIZE + 1,-1);
         vector<int> res(SIZE,-1);
         
@@ -20,28 +20,31 @@ public:
                     isGCD[j][i] = 1;
                 }
         
+        for(int i = 1;i <= 50;i++)
+            mp[i] = -1;
+        
         function<void(int,int)> dfs = [&](int node,int curtime) {
             int ret = SIZE;
             int oldNode = -1;
-            if(mp.count(nums[node]))
-                oldNode = mp[nums[node]];
+            oldNode = mp[nums[node]];
             visitedTime[node] = curtime;
-            for(auto it = mp.begin();it != mp.end();it++){
-                int val = it->first, ancestor = it->second;
-                if(isGCD[nums[node]][val] == 1)
-                    if(visitedTime[ret] < visitedTime[ancestor])
-                        ret = ancestor;
+            
+            for(int i = 1;i <= 50;i++){
+                if(mp[i] == -1) continue;
+                if(isGCD[nums[node]][i] == 1)
+                    if(visitedTime[ret] < visitedTime[mp[i]])
+                        ret = mp[i];
             }
+            
             if(ret != SIZE)
                 res[node] = ret;
             mp[nums[node]] = node;
             for(int nei : adjacents[node])
                 if(visitedTime[nei] == -1)
                     dfs(nei,curtime + 1);
-            if(oldNode != -1)
-                mp[nums[node]] = oldNode;
-            else
-                mp.erase(nums[node]);
+            
+            mp[nums[node]] = oldNode;
+            
             return;
         };
         
