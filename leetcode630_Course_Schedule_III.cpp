681a580112b45608b246381ccb34048946070bec
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-07-28 09:46:36 -0400

    submit leetcode daily challenge solution 07/29/2021

---
+++ leetcode630_Course_Schedule_III.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int scheduleCourse(vector<vector<int>>& courses) {
+        const int SIZE = courses.size();
+        priority_queue<int> pq;
+        int curTime = 0;
+        
+        sort(courses.begin(),courses.end(),[](auto& lhs,auto& rhs){
+            return lhs[1] < rhs[1];
+        });
+        
+        for(vector<int>& c : courses){
+            if(c[0] > c[1]) continue;
+            curTime+= c[0];
+            pq.push(c[0]);
+            if(curTime > c[1]){
+                curTime-= pq.top();
+                pq.pop();
+            }
+        }
+        
+        return pq.size();
+    }
+};
