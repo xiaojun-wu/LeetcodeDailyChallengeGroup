b5ee908b7805c3a5cbcb496660d3d19b4ede9539
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-07-28 08:11:19 -0400

    submit leetcode daily challenge solution 07/26/2021

---
+++ leetcode1353_Maximum_Number_of_Events_That_Can_Be_Attended.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int maxEvents(vector<vector<int>>& events) {
+        /*
+        heap + sort.
+        TC : O(N logN), SC : O(N)
+        */
+        const int SIZE = events.size();
+        events.push_back({100001,100002});
+        priority_queue<int,vector<int>,greater<int>> pq;
+        int res = 0, preTime = 0;
+        
+        sort(events.begin(),events.end(),[](auto& lhs,auto& rhs){
+            if(lhs[0] != rhs[0])
+                return lhs[0] < rhs[0];
+            return lhs[1] < rhs[1];
+        });
+        
+        for(vector<int>& e : events){
+            int startTime = e[0], endTime = e[1];
+            while(!pq.empty() && preTime < startTime){
+                if(pq.top() >= preTime){
+                    res++;
+                    preTime++;
+                }
+                pq.pop();
+            }
+            pq.push(endTime);
+            preTime = startTime;
+        }
+        
+        return res;
+    }
+};
