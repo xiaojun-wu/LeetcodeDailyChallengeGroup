548e368f493bfa8053c304aa47d1b7afe6d0147e
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-07-28 08:46:45 -0400

    submit leetcode daily challenge solution 07/27/2021

---
+++ leetcode1834_Single-Threaded_CPU.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> getOrder(vector<vector<int>>& tasks) {
+        /*
+        heap + sort. TC : O(N log N), SC : O(N)
+        */
+        const int SIZE = tasks.size();
+        vector<int> res;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        
+        for(int i = 0;i < SIZE;i++)
+            tasks[i].push_back(i);
+        
+        sort(tasks.begin(),tasks.end(),[](auto& lhs,auto& rhs){
+            return lhs[0] < rhs[0];
+        });
+        
+        for(int i = 0,startTime = 0;i < SIZE;i++){
+            if(pq.empty())
+                startTime = max(startTime,tasks[i][0]);
+            while(i < SIZE && tasks[i][0] <= startTime)
+                pq.emplace(tasks[i][1],tasks[i++][2]);
+            i--;
+            startTime+= pq.top().first;
+            res.push_back(pq.top().second);
+            pq.pop();
+        }
+        while(!pq.empty()){
+            res.push_back(pq.top().second);
+            pq.pop();
+        }
+        
+        return res;
+    }
+};
