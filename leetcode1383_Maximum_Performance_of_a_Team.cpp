4dbeb493432f4865553e5e3df9ca92386f861634
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-07-28 10:34:54 -0400

    submit leetcode daily challenge solution 07/31/2021

---
+++ leetcode1383_Maximum_Performance_of_a_Team.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int maxPerformance(int n, vector<int>& speed, vector<int>& efficiency, int k) {
+        const int SIZE = speed.size(), MOD = 1e9 + 7;
+        long long res = 0, totalSpeed = 0;
+        vector<pair<int,int>> arrs;
+        priority_queue<int,vector<int>,greater<int>> pq;
+        
+        for(int i = 0;i < SIZE;i++)
+            arrs.emplace_back(efficiency[i],speed[i]);
+        
+        sort(arrs.begin(),arrs.end(),greater<pair<int,int>>());
+        
+        for(int i = 0;i < k;i++){
+            pq.push(arrs[i].second);
+            totalSpeed+= (long long)arrs[i].second;
+            res = max(res,totalSpeed * (long long)arrs[i].first);
+        }
+        
+        for(int i = k;i < SIZE;i++){
+            if(pq.top() < arrs[i].second){
+                totalSpeed+= arrs[i].second - pq.top();
+                pq.pop();
+                pq.push(arrs[i].second);
+            }
+            res = max(res,totalSpeed * (long long)arrs[i].first);
+        }
+        
+        return res % MOD;
+    }
+};
