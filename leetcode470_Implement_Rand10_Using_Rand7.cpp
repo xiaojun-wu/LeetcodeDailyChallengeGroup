d83d4356b9611da0682f1b42b321d620ff99bebc
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-08-05 09:55:51 -0400

    submit leetcode daily challenge solution 08/06/2021

---
+++ leetcode470_Implement_Rand10_Using_Rand7.cpp
@@ -0,0 +1,14 @@
+// The rand7() API is already defined for you.
+// int rand7();
+// @return a random integer in the range 1 to 7
+
+class Solution {
+public:
+    int rand10() {
+        int val = 0;
+        do{
+            val = ((rand7() - 1) * 7) + rand7();
+        }while(val > 40);
+        return val % 10 + 1;
+    }
+};
