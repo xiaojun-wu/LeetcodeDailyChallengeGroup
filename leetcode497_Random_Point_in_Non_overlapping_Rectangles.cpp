000daf182dbf0605ef27723cdb2d24a266cf61e7
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-08-05 08:23:20 -0400

    submit leetcode daily challenge solution 08/05/2021

---
+++ leetcode497_Random_Point_in_Non_overlapping_Rectangles.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    Solution(vector<vector<int>>& rects) {
+        totalWeights = 0;
+        this->itr = rects.begin();
+        for(vector<int>& rect : rects){
+            int area = (rect[2] - rect[0] + 1) * (rect[3] - rect[1] + 1);
+            totalWeights+= area;
+            weights.push_back(totalWeights);
+        }
+    }
+    
+    vector<int> pick() {
+        int weight = rand() % totalWeights + 1;
+        int idx = lower_bound(weights.begin(),weights.end(),weight) - weights.begin();
+        auto nitr = itr + idx;
+        int width = (*nitr)[2] - (*nitr)[0], length = (*nitr)[3] - (*nitr)[1];
+        int w = rand() % (width + 1), l = rand() % (length + 1);
+        return {(*nitr)[0] + w,(*nitr)[1] + l};
+    }
+private:
+    int totalWeights;
+    vector<int> weights;
+    vector<vector<int>>::iterator itr;
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(rects);
+ * vector<int> param_1 = obj->pick();
+ */
