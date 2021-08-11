b71d6fb851f92ffb48ed513a3b01f47fa7fc2453
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-08-11 15:55:39 -0400

    submit leetcode daily challenge solution 08/13/2021

---
+++ leetcode1824_Minimum_Sideway_Jumps.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minSideJumps(vector<int>& obstacles) {
+        const int SIZE = obstacles.size(), MAXVAL = SIZE * 2;
+        vector<int> dp(3,1), next(3,0);
+        dp[1] = 0;
+        
+        for(int i = 1;i < SIZE;i++){
+            next[0] = dp[0];
+            next[1] = dp[1];
+            next[2] = dp[2];
+            if(obstacles[i] != 0)
+                next[obstacles[i] - 1] = MAXVAL;
+            int minVal = min({next[0],next[1],next[2]});
+            for(int j = 0;j < 3;j++)
+                if(j != obstacles[i] - 1)
+                    next[j] = min(next[j],minVal + 1);
+            swap(dp,next);
+        }
+        
+        return min({dp[0],dp[1],dp[2]});
+    }
+};
