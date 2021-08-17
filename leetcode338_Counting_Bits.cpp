2b0e5dbb5f9bd4276ec63ff007c99cae5627b34c
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-08-17 12:05:10 -0400

    submit leetcode daily challenge solution 08/17/2021

---
+++ leetcode338_Counting_Bits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> res(n + 1,0);
+        
+        for(int i = 0;i <= n / 2;i++){
+            if((i << 1) <= n)
+                res[i << 1] = res[i];
+            if((i << 1) + 1 <= n)
+                res[(i << 1) + 1] = res[i] + 1;
+        }
+        
+        return res;
+    }
+};
