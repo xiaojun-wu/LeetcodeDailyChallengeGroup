caae84d1c3cc6b7131300bec4cfb7bd3a1462817
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-08-17 13:48:23 -0400

    submit leetcode daily challenge solution 08/22/2021

---
+++ leetcode137_Single_Number_II.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int res = 0, sum = 0;
+        for(int i = 0;i < 32;i++){
+            sum = 0;
+            for(int n : nums)
+                sum+= (n >> i & 1);
+            res+= ((sum % 3) << i);
+        }
+        
+        return res;
+    }
+};
