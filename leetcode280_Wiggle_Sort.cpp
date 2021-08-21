f52a90b561a71b7e5b09062037afadfb20fdfaba
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-08-21 13:40:48 -0400

    submit leetcode daily challenge solution 08/20/2021

---
+++ leetcode280_Wiggle_Sort.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+
+        for(int i = 1;i < nums.size() - 1;i+= 2){
+            swap(nums[i],nums[i + 1]);
+        }
+
+        return;
+    }
+};
