d39ed181991c09032b489490e138df384b00f3c7
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-08-24 11:57:17 -0400

    submit leetcode daily challenge solution 08/23/2021

---
+++ leetcode105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        const int SIZE = preorder.size();
+        vector<TreeNode*> stack;
+        stack.push_back(new TreeNode(preorder[0]));
+        TreeNode* cur = stack.back(), *root = cur;
+        
+        for(int i = 1,j = 0;i < preorder.size();i++){
+            if(cur->val != inorder[j]){
+                cur->left = new TreeNode(preorder[i]);
+                cur = cur->left;
+                stack.push_back(cur);
+            }
+            else{
+                while(!stack.empty() && stack.back()->val == inorder[j]){
+                    cur = stack.back();
+                    stack.pop_back();
+                    j++;
+                }
+                cur->right = new TreeNode(preorder[i]);
+                cur = cur->right;
+                stack.push_back(cur);
+            }
+        }
+        
+        return root;
+    }
+};
