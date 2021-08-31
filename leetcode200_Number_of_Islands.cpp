3dec70f1f7509899d52efff10c3394ea7163bf2a
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-08-31 11:21:52 -0400

    submit leetcode daily challenge solution 08/30/2021

---
+++ leetcode200_Number_of_Islands.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        const int ROW = grid.size(), COL = grid[0].size();
+        vector<vector<int>> dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+        int res = 0;
+        
+        function<void(int,int)> dfs = [&](int r,int c) {
+            grid[r][c] = '0';
+            for(vector<int>& d : dirs){
+                int nr = r + d[0], nc = c + d[1];
+                if(nr < 0 || nc < 0 || nr == ROW || nc == COL || grid[nr][nc] == '0')
+                    continue;
+                dfs(nr,nc);
+            }
+        };
+        
+        for(int r = 0;r < ROW;r++)
+            for(int c = 0;c < COL;c++)
+                if(grid[r][c] == '1'){
+                    res++;
+                    dfs(r,c);
+                }
+        
+        return res;
+    }
+};
