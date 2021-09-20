46b30885d77c30990c5258ffb10af16165536309
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-09-20 19:21:27 -0400

    submit leetcode daily challenge solution 09/20/2021

---
+++ leetcode1039_Minimum_Score_Triangulation_of_Polygon.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minScoreTriangulation(vector<int>& values) {
+        const int SIZE = values.size();
+        vector<vector<int>> dp(SIZE,vector<int>(SIZE,INT_MAX));
+        
+        function<int(int,int)> dfs = [&](int left,int right) ->int {
+            if(right - left < 2)
+                return 0;
+            if(dp[left][right] != INT_MAX)
+                return dp[left][right];
+            
+            int minProduct = INT_MAX;
+            
+            for(int i = left + 1;i < right;i++)
+                minProduct = min(minProduct,values[left] * values[right] * values[i] + dfs(left,i) + dfs(i,right));
+            dp[left][right] = minProduct;
+            
+            return minProduct;
+        };
+        
+        return dfs(0,SIZE -1);
+    }
+};
