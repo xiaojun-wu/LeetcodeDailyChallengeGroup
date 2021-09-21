e771d79f550e4adfc1fc9e8a78bbcbdfc8423e7a
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-09-21 00:20:35 -0400

    submit leetcode daily challenge solution 09/24/2021

---
+++ leetcode673_Number_of_Longest_Increasing_Subsequence.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int findNumberOfLIS(vector<int>& nums) {
+        const int SIZE = nums.size();
+        long maxLen = 0, count = 0;
+        vector<long> dp(SIZE + 1,1);
+        vector<int> len(SIZE,1);
+        dp[0] = 1;
+
+        for(int i = 0;i < SIZE;i++)
+            for(int j = i - 1;j >= 0;j--)
+                if(nums[i] > nums[j]){
+                    if(len[j] >= len[i]){
+                        dp[i] = dp[j];
+                        len[i] = len[j] + 1;
+                    }
+                    else if(len[j] + 1 == len[i] && dp[i] + dp[j] <= INT_MAX)
+                        dp[i]+= dp[j];
+                }
+
+        for(int i = 0;i < SIZE;i++){
+            if(maxLen < len[i]){
+                maxLen = len[i];
+                count = dp[i];
+            }
+            else if(maxLen == len[i] && count + dp[i] <= INT_MAX)
+                count+= dp[i];
+        }
+
+        return count;
+    }
+};
