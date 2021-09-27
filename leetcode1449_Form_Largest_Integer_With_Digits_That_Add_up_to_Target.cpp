e085db484bf61ff3649e73cd27e5015ba8768e00
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-09-27 17:38:10 -0400

    submit leetcode daily challenge solution 10/02/2021

---
+++ leetcode1449_Form_Largest_Integer_With_Digits_That_Add_up_to_Target.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string largestNumber(vector<int>& cost, int target) {
+        const int SIZE = cost.size();
+        string res = "";
+        vector<int> dp(target + 1,0);
+        vector<int> indice(target + 1,-1);
+        dp[0] = 1;
+        
+        for(int t = 0;t <= target;t++)
+            for(int i = 8;i >= 0;i--)
+                if(cost[i] <= t && dp[t - cost[i]] && dp[t - cost[i]] + 1 > dp[t]){
+                    dp[t] = dp[t - cost[i]] + 1;
+                    indice[t] = i + 1;
+                }
+        
+        if(dp[target] == 0)
+            return "0";
+        
+        while(target > 0){
+            res+= indice[target] + '0';
+            target-= cost[indice[target] - 1];
+        }
+        
+        return res;
+    }
+};
