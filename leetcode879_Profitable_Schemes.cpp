9296d44406f62b2495eb869389163d4260e8699b
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-09-27 15:23:09 -0400

    submit leetcode daily challenge solution 09/30/2021

---
+++ leetcode879_Profitable_Schemes.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) {
+        const int SIZE = group.size(), MOD = 1e9 + 7;
+        vector<vector<int>> dp(n + 1,vector<int>(minProfit + 1,0));
+        int res = 0;
+        dp[0][0] = 1;
+        
+        for(int i = 0;i < SIZE;i++){
+            int pf = profit[i], g = group[i];
+            for(int j = n - g;j >= 0;j--){
+                for(int k = minProfit;k >= 0;k--){
+                    if(dp[j][k]){
+                        int m = min(pf + k,minProfit);
+                        dp[j + g][m] = (dp[j + g][m] + dp[j][k]) % MOD;
+                    }
+                }
+            }
+        }
+        
+        for(int i = 0;i <= n;i++)
+            res = (res + dp[i][minProfit]) % MOD;
+        
+        return res;
+    }
+};
