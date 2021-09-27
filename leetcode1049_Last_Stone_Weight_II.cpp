aed6ee2a2bf76a7d0bd6fefd6a917303bbee591d
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-09-27 14:57:34 -0400

    submit leetcode daily challenge solution 09/27/2021

---
+++ leetcode1049_Last_Stone_Weight_II.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int lastStoneWeightII(vector<int>& stones) {
+        int totalSum = accumulate(stones.begin(),stones.end(),0);
+        const int HALF = totalSum / 2;
+        vector<int> dp(HALF + 1,0);
+        dp[0] = 1;
+        
+        for(int n : stones){
+            for(int t = HALF - n;t >= 0;t--)
+                if(dp[t])
+                    dp[t + n] = 1;
+            if(dp[HALF])
+                return totalSum - 2 * HALF;
+        }
+        
+        for(int t = HALF;t >= 0;t--)
+            if(dp[t])
+                return totalSum - 2 * t;
+        
+        return -1;
+    }
+};
