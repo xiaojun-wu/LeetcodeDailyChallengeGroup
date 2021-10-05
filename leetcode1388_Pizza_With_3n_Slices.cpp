0fd8d12bf255a7766d0268bd45b10f3ea5acf9b0
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-10-05 17:32:53 -0400

    submit leetcode daily challenge solution 10/07/2021

---
+++ leetcode1388_Pizza_With_3n_Slices.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxSizeSlices(vector<int>& slices) {
+        const int SIZE = slices.size();
+        
+        function<int(int,int)> helper = [&](int start,int end) ->int {
+            vector<vector<int>> dp(SIZE,vector<int>(SIZE / 3,0));
+            while(start < end){
+                dp[start][0] = slices[start];
+                if(start - 1 >= 0)
+                    dp[start][0] = max(dp[start][0],dp[start -1][0]);
+                if(start - 2 < 0){
+                    start++;
+                    continue;
+                }
+                for(int i = 1;i < SIZE / 3;i++){
+                    dp[start][i] = max(dp[start - 2][i - 1] + slices[start],dp[start - 1][i]);
+                }
+                start++;
+            }
+            return dp[end - 1][SIZE / 3 - 1];
+        };
+        
+        return max(helper(0,SIZE - 1),helper(1,SIZE));
+    }
+};
