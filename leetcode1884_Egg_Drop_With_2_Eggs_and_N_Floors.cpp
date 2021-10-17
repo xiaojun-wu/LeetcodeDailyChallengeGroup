f5f31612c48e6457fe353f2914e084b39e0546fa
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-10-17 19:23:44 -0400

    submit leetcode daily challenge solution 10/13/2021

---
+++ leetcode1884_Egg_Drop_With_2_Eggs_and_N_Floors.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int twoEggDrop(int n) {
+        vector<int> dp(n + 1,n);
+        dp[0] = dp[1] = 1;
+        
+        for(int i = 2;i <= n;i++)
+            for(int j = i - 1;j >= 1;j--)
+                dp[i] = min(dp[i],max(j,dp[i - j] + 1));
+        return dp[n];
+    }
+};
+/*
+if we choose first drop if m:
+1. If egg break in m floor, we have to do up to m - 1 more drops, total m drops needs.
+2. If egg wasn't break, we can image m floor is 0, and n is n - m floor, start this process over by determine a egg in n - m floors + 1.
+
+DP: TC : O(N^2), SC : O(N)
+*/
