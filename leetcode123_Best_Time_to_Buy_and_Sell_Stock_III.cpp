e9aa93a6eb79c01e0f2c17274e40ce264b7e5d5a
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-10-17 20:26:33 -0400

    submit leetcode daily challenge solution 10/14/2021

---
+++ leetcode123_Best_Time_to_Buy_and_Sell_Stock_III.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        const int SIZE = prices.size(), MAXPROFIT = 1e6;
+        int firstBuy = INT_MIN, firstSell = 0;
+        int secondBuy = INT_MIN, secondSell = 0;
+        
+        for(int price : prices){
+            // update second sell
+            secondSell = max(secondSell,secondBuy + price);
+            // update second buy
+            secondBuy = max(secondBuy,firstSell - price);
+            
+            // update first sell
+            firstSell = max(firstSell,firstBuy + price);
+            // update first buy
+            firstBuy = max(firstBuy, -price);
+        }
+        
+        return max(secondSell,firstSell);
+    }
+};
