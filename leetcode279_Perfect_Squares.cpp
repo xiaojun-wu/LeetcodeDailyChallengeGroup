4a490311b0461b1e712d429e3942b38c3f76b2bc
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-10-17 20:28:37 -0400

    submit leetcode daily challenge solution 10/11/2021

---
+++ leetcode279_Perfect_Squares.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> dp(n + 1,n);
+        vector<int> squares;
+        dp[0] = 0;
+        
+        for(int i = 1;i * i <= n;i++)
+            squares.push_back(i);
+        
+        for(int i = 1;i <= n;i++){
+            if((int)(sqrt(i)) * (int)(sqrt(i)) == i)
+                dp[i] = 1;
+            else{
+                for(int s : squares){
+                    if(s * s > i)
+                        break;
+                    dp[i] = min(dp[i],dp[i - s * s] + 1);
+                }
+            }
+        }
+        
+        return dp[n];
+    }
+};
