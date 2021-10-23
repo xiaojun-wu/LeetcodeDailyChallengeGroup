432490cf4d3d3e0057ce80029174040ee1e884a4
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-10-23 15:53:47 -0400

    submit leetcode daily challenge solution 10/24/2021

---
+++ leetcode1987_Number_of_Unique_Good_Subsequences.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numberOfUniqueGoodSubsequences(string binary) {
+        const int LEN = binary.length(), MOD = 1e9 + 7;
+        long one = 0, zero = 0, hasZero = 0;
+        
+        for(int i = LEN - 1;i >= 0;i--){
+            long val = (one + zero + 1) % MOD;
+            hasZero|= binary[i] == '0';
+            if(binary[i] == '0')
+                zero = val;
+            else
+                one = val;
+        }
+        
+        return (one + hasZero) % MOD;
+    }
+};
