bc70b6591c92de5bc1297ef6b53128f23d1c8c74
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-10-23 01:13:32 -0400

    submit leetcode daily challenge solution 10/23/2021

---
+++ leetcode940_Distinct_Subsequences_II.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int distinctSubseqII(string S) {
+        const int LEN = S.length(), MOD = 1e9 + 7;
+        int presum[2001] = {0};
+        int mp[26] = {0};
+        
+        for(int i = 0;i < LEN;i++){
+            int cur = 0;
+            char c = S[i];
+            if(mp[c - 'a'] == 0)
+                cur++;
+            int pre = 0;
+            if(mp[c -  'a'] > 1)
+                pre = presum[mp[c -  'a'] - 1];
+            presum[i + 1] = (cur + (presum[i] * 2) % MOD + MOD - pre) % MOD;
+            mp[c - 'a'] = i + 1;
+        }
+        
+        return presum[LEN];
+    }
+};
