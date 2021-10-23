35c8599a3024e1f5d0da95f0cb8920d6fdef4ca4
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-10-22 21:27:42 -0400

    submit leetcode daily chanllenge solution 10/20/20

---
+++ leetcode1575_Count_All_Possible_Routes.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int countRoutes(vector<int>& locations, int start, int finish, int fuel) {
+        const int SIZE = locations.size(), MOD = 1e9 + 7;
+        int dp[201][101] = {0};
+        dp[fuel][start] = 1;
+        int res = 0;
+        
+        for(int f = fuel;f >= 1;f--){
+            for(int i = 0;i < SIZE;i++){
+                if(dp[f][i] == 0)
+                    continue;
+                for(int j = 0;j < SIZE;j++){
+                    if(i == j)
+                        continue;
+                    int dis = abs(locations[i] - locations[j]);
+                    if(f >= dis)
+                        dp[f - dis][j] = (dp[f - dis][j] + dp[f][i]) % MOD;
+                }
+            }
+        }
+        
+        for(int f = 0;f <= fuel;f++)
+            res = (res + dp[f][finish]) % MOD;
+        
+        return res;
+    }
+};
