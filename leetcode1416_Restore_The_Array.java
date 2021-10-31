61ca75357590e26105088b3be942e69a2b87c85b
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-10-31 16:11:38 -0400

    submit leetcode daily challenge solution 10/29/2021

---
+++ leetcode1416_Restore_The_Array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numberOfArrays(String s, int k) {
+        final int LEN = s.length(), MOD = 1000000000 + 7, KLEN = Integer.toString(k).length();
+        int[] dp = new int[LEN + 1];
+        dp[0] = 1;
+        
+        for(int i = 0;i < LEN;i++){
+            for(int j = 0;j < KLEN && i - j >= 0;j++){
+                if(s.charAt(i - j) == '0')
+                    continue;
+                String numStr = s.substring(i - j,i + 1);
+                long num = Long.parseLong(numStr);
+                if(num > (long)(k))
+                    break;
+                dp[i + 1] = (dp[i + 1] + dp[i - j]) % MOD;
+            }
+        }
+        
+        return dp[LEN];
+    }
+}
