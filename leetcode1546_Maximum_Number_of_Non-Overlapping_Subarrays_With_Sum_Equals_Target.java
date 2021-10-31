e9b5b51e0129b2e3cfabacf94153f89a96f7a342
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-10-31 02:12:01 -0400

    submit leetcode daily challenge solution 10/272021

---
+++ leetcode1546_Maximum_Number_of_Non-Overlapping_Subarrays_With_Sum_Equals_Target.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxNonOverlapping(int[] nums, int target) {
+        final int SIZE = nums.length;
+        Map<Integer,Integer> map = new HashMap<>();
+        int left = -1, res = 0;
+        map.put(0,-1);
+        
+        for(int i = 0, sum = 0;i < SIZE;i++){
+            sum+= nums[i];
+            int idx = map.getOrDefault(sum - target,-2);
+            if(idx >= left){
+                res++;
+                left = i;
+            }
+            map.put(sum,i);
+        }
+        
+        return res;
+    }
+}
