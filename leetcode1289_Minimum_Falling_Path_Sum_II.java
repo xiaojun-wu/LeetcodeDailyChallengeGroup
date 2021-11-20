30a1622be69a135522bd620c5ed56ece2cc78947
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-11-19 19:14:49 -0500

    submit leetcode daily challenge solution 11/07/2021

---
+++ leetcode1289_Minimum_Falling_Path_Sum_II.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minFallingPathSum(int[][] grid) {
+        final int SIZE = grid.length;
+        if(SIZE == 1)
+            return grid[0][0];
+        int min1 = 0, min2 = 0, idx = -1;
+        
+        for(int r = 0;r < SIZE;r++){
+            int curmin1 = 200000;
+            int curmin2 = 200000;
+            int curidx = -1;
+            for(int c = 0;c < SIZE;c++){
+                int cur = 200000;
+                if(c == idx){
+                    cur = grid[r][c] + min2;
+                }
+                else
+                    cur = grid[r][c] + min1;
+                if(cur < curmin1){
+                    curmin2 = curmin1;
+                    curmin1 = cur;
+                    curidx = c;
+                }
+                else if(cur < curmin2)
+                    curmin2 = cur;
+            }
+            min1 = curmin1;
+            min2 = curmin2;
+            idx = curidx;
+        }
+        
+        return min1;
+    }
+}
