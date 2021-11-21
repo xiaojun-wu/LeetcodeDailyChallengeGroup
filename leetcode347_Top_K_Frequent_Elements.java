5c939a87df03d05e2320c277866fbd88a8b751cb
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-11-21 01:06:57 -0500

    submit leetcode daily challenge solution 11/20/2021

---
+++ leetcode347_Top_K_Frequent_Elements.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        final int SIZE = nums.length;
+        int res[] = new int[k];
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int idx = 0;
+        
+        for(int i = 0;i < SIZE;i++)
+            map.put(nums[i],map.getOrDefault(nums[i],0) + 1);
+        
+        int ps[][] = new int[map.size()][2];
+        
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+            ps[idx][0] = entry.getValue();
+            ps[idx++][1] = entry.getKey();
+        }
+        
+        Arrays.sort(ps,(a,b) -> b[0] - a[0]);
+        
+        for(int i = 0;i < k;i++)
+            res[i] = ps[i][1];
+        
+        return res;
+    }
+}
