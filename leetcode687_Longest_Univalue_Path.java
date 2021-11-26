01c9b096afc4c4d45bd4900182ef65b009d40648
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-11-25 20:18:58 -0500

    submit leetcode daily challenge solution 11/25/2021

---
+++ leetcode687_Longest_Univalue_Path.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int res;
+    public int longestUnivaluePath(TreeNode root) {
+        if(root == null)
+            return 0;
+        this.res = 0;
+        dfs(root);
+        return res - 1;
+    }
+    private int dfs(TreeNode node){
+        if(node == null)
+            return 0;
+        int leftLen = dfs(node.left);
+        int rightLen = dfs(node.right);
+        if(node.left != null && node.left.val != node.val)
+            leftLen = 0;
+        if(node.right != null && node.right.val != node.val)
+            rightLen = 0;
+        res = Math.max(res,leftLen + rightLen + 1);
+        // System.out.println("val : " + node.val + ", leftLen : " + leftLen + ", rightLen : " + rightLen + ", res : " + res);
+        return Math.max(leftLen,rightLen) + 1;
+    }
+}
