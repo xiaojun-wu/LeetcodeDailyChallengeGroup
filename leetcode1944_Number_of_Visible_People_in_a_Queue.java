ec93f8b7455d15f1e1b323709ef49fb085e7034e
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-12-19 00:46:18 -0500

    submit leetcode daily challenge solution 12/04/2021

---
+++ leetcode1944_Number_of_Visible_People_in_a_Queue.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] canSeePersonsCount(int[] heights) {
+        int SIZE = heights.length;
+        Stack<Integer> stack = new Stack<>();
+        int res[] = new int[SIZE];
+        
+        for(int i = SIZE - 1;i >= 0;i--){
+            while(!stack.empty() && stack.peek() < heights[i]){
+                res[i]++;
+                stack.pop();
+            }
+            if(!stack.empty())
+                res[i]++;
+            stack.push(heights[i]);
+        }
+        
+        return res;
+    }
+}
