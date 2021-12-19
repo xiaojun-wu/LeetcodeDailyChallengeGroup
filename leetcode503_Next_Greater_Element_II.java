9a748a9f8ad7a039470b74d95e978786e471d3cc
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-12-19 13:41:28 -0500

    submit leetcode daily challenge solution 12/15/2021

---
+++ leetcode503_Next_Greater_Element_II.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int SIZE = nums.length;
+        int res[] = new int[SIZE];
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int i = SIZE - 1;i >= 0;i--){
+            while(!stack.empty() && stack.peek() <= nums[i])
+                stack.pop();
+            if(!stack.empty())
+                res[i] = stack.peek();
+            else
+                res[i] = Integer.MIN_VALUE;
+            stack.push(nums[i]);
+        }
+        
+        for(int i = SIZE - 1;i >= 0;i--){
+            while(!stack.empty() && stack.peek() <= nums[i])
+                stack.pop();
+            if(!stack.empty() && res[i] == Integer.MIN_VALUE && stack.peek() > nums[i])
+                res[i] = stack.peek();
+            if(res[i] == Integer.MIN_VALUE)
+                res[i] = -1;
+        }
+        
+        return res;
+    }
+}
