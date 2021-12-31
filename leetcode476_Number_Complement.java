4c95774ddb0e7fccde201b0e8318ae3324f6413f
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-12-31 18:29:41 -0500

    submit leetcode daily challenge 12/27/2021

---
+++ leetcode476_Number_Complement.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findComplement(int num) {
+        int res = 0;
+        int bit = 0;
+        while(num > 0){
+            if((num & 1) != 1)
+                res+= (1 << bit);
+            bit++;
+            num>>= 1;
+        }
+        return res;
+    }
+}
