e382b27cf10e067bbdb14c70efaa0f372f352575
xiaojun-wu <wuxiaojun1991@gmail.com>
2021-12-31 18:15:44 -0500

    submit leetcode daily challenge solution 12/3

---
+++ leetcode450_Delete_Node_in_a_BST.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        return dfs(root,key);
+    }
+    public TreeNode dfs(TreeNode node,int key){
+        if(node == null)
+            return null;
+        if(node.val > key){
+            node.left = dfs(node.left,key);
+        }
+        else if(node.val < key){
+            node.right = dfs(node.right,key);
+        }
+        else{
+            if(node.right == null)
+                return node.left;
+            TreeNode candidate = leftMost(node.right);
+            candidate.left = node.left;
+            return node.right;
+        }
+        return node;
+    }
+    public TreeNode leftMost(TreeNode node){
+        if(node.left == null)
+            return node;
+        return leftMost(node.left);
+    }
+}
